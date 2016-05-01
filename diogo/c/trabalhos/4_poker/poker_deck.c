c1cdcbaaeb322a5a16dab6ee7b416e17a76f729a
MrAnyone <m3rc3r99@gmail.com>
2016-05-01 10:43:34 -0300

    Add more comments

--- diogo/c/trabalhos/4_poker/poker_deck.c
+++ diogo/c/trabalhos/4_poker/poker_deck.c
@@ -34,28 +34,49 @@ Card getRandCard() {
 //generates the card pile
 //can be used to reset the pile
 void generateCardPile() {
+    //z is the array index
+    //i is the number (1 - 13)
+    //j is the suit (1 - 4)
     for(z = 0, i = 1; i < 14; i++) {
         for(z, j = 1; j < 5; j++, z++) {
             cardPile[z].num = i;
             cardPile[z].suit = j;
         }
     }
+
+    //the index in the cardPile array
+    //since the pile is full, the index
+    //will be 51
     cardCount = z;
 }
 
 //pick a random card of
 //from card pile
 Card pickCard() {
+
+    //select a random card
+    //based in teh cardPile
+    //size
     randVar = (double)rand()/RAND_MAX;
     i = cardCount * randVar;
     c = cardPile[i];
-
+    
+    //shift all the cards to the left from
+    //the choosen one
     for(j = i + 1; j <= cardCount; j++) {
         cardPile[j - 1] = cardPile[j];
     }
-
+    
+    //decrement the cardPile
+    //preventing dupe cards
     cardCount -= 1;
 
+    //warnings that the cardPile is empty
+    if(!cardCount) {
+        printf("\nThe cardPile is empty!");
+    }
+    
+    //return the picked card
     return c;
 }
 

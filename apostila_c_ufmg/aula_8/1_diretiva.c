81c5709f52cdd9e0e3465c2e0fc51f31f22abadf
MrAnyone <m3rc3r99@gmail.com>
2016-05-06 08:52:46 -0300

    Update 1_diretiva.c

--- apostila_c_ufmg/aula_8/1_diretiva.c
+++ apostila_c_ufmg/aula_8/1_diretiva.c
@@ -3,7 +3,23 @@
 #define min(A,B) ((A<B) ? (A):(B))
 #define PRINT(i) printf(" %d \n", i)
 #define HAH "Isso sera deletado"
+#define TEST "it"
+
+#ifdef HAH
+    #define X 62
+#endif
+
+#ifdef YU
+#elif UY
+#else
+    #define W "%d|%d"
+#endif
+
 #undef HAH
+
+#ifndef HAH
+    #define Y 23
+#endif
 
 int main () {
     int i = 4;
@@ -11,10 +27,8 @@ int main () {
     int x = max(i, j); //essa sentença ira ser substituida
     int y = min(i, j); //por x = ((i) > (j) ? (i):(j));
 
-    PRINT(x + y); //essa sentença sera substituida por 
-                  //printf(" %d \n", (x + y));
-
-
-    printf("%d|%d", x, y);
+    PRINT(Y + x + y + X); //essa sentença sera substituida por 
+                          //printf(" %d \n", (23 + x + y + 62));
+    printf(W, x, y);
     return 0;
 }

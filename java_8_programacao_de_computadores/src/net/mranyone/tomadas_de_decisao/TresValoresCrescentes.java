3e6154bf94a951ea8f5c76826d2e6de7d9268456
MrAnyone <m3rc3r99@gmail.com>
2016-05-09 20:39:46 -0300

    Add TresValoresCrescentes.java

---
+++ java_8_programacao_de_computadores/src/net/mranyone/tomadas_de_decisao/TresValoresCrescentes.java
@@ -0,0 +1,13 @@
+package net.mranyone.tomadas_de_decisao;
+
+public class TresValoresCrescentes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

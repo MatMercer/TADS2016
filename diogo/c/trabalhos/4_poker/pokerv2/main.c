4e2aeb990d92ce7f65979090675cb649f8f2292a
MrAnyone <m3rc3r99@gmail.com>
2016-05-15 13:03:26 -0300

    Add getStraight

--- diogo/c/trabalhos/4_poker/pokerv2/main.c
+++ diogo/c/trabalhos/4_poker/pokerv2/main.c
@@ -12,17 +12,19 @@ int main() {
     c.n = 8;
     c.s = 3;
 
-    c1.n = 3;
+    c1.n = 7;
     c1.s = 3;
 
-    c2.n = 5;
+    c2.n = 6;
     c2.s = 3;
 
-    c3.n = 4;
+    c3.n = 5;
     c3.s = 3;
 
-    c4.n = 2;
+    c4.n = 4;
     c4.s = 3;
-    printf("%x", getFlush(c, c1, c2, c3, c4));
+    printf("%x\n", getFlush(c, c1, c2, c3, c4));
+
+    printf("%x\n", getStraight(c, c1, c2, c3, c4));
     return 0;
 }
--- diogo/c/trabalhos/4_poker/pokerv2/texasholdem.c
+++ diogo/c/trabalhos/4_poker/pokerv2/texasholdem.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
 #include "pokercard.h"
 
+/*
+ * Checks for a flush, if false, returns
+ * 0, if true, returns the suit binary code
+*/
 int getFlush(Card c1, Card c2, Card c3, Card c4, Card c5) {
     return (getCardCode(c1) & getCardCode(c2) & getCardCode(c3) & getCardCode(c4) & getCardCode(c5) & 0xF000);
+}
+
+/*
+ * Checks for a straight, if false, returns
+ * 0, if true, returns the straight power
+*/
+int getStraight(Card c1, Card c2, Card c3, Card c4, Card c5) {
+    int i = 0;
+
+    /*
+     * Get only the card number
+    */
+    int n = (((getCardCode(c1) | getCardCode(c2) | getCardCode(c3) | getCardCode(c4) | getCardCode(c5)) & 0xFFFF0000) >> 16);
+    int res = n;
+   
+    /*
+     * Removes all the right zeros
+    */
+    while((res & 0x1) == 0) {
+        res >>= 1;
+        i++;
+    }
+    
+    /* 
+     * Checks for consecutive 1's
+     * until res is equals 1
+    */
+    while((res & 0x1) == 1) {
+        if(res == 1)
+            return n;
+        else
+            res >>= 1;
+    }
+
+    return 0;
 }

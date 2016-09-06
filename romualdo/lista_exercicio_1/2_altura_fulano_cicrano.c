5305dff180e82d5b70bd89bfabbe9897f70e45af
MrAnyone <m3rc3r99@gmail.com>
2016-09-06 07:54:49 -0300

    Add romualdo > lista_exercicio_1

---
+++ romualdo/lista_exercicio_1/2_altura_fulano_cicrano.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int alturaFulano = 170;
+    int alturaCicrano = 150;
+    int crescimentoFulano = 2;
+    int crescimentoCicrano = 4;
+    int anos;
+    
+    for(anos = 0; alturaCicrano <= alturaFulano; anos += 1, alturaFulano += crescimentoFulano, alturaCicrano += crescimentoCicrano) {
+        printf("--- ano %d ---\n", anos);
+        printf("Fulano: %d cm.\n", alturaFulano);
+        printf("Cicrano: %d cm.\n", alturaCicrano);
+    }
+
+    printf("\nDemora %d anos para Cicrano ficar mais alto que Fulano.\n", anos);
+}

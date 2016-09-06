5db3260af5ace7b9c2ea4b969016af4dc7fa82be
MrAnyone <m3rc3r99@gmail.com>
2016-09-06 07:58:01 -0300

    Add information to 2_altura_fulano_cicrano

--- romualdo/lista_exercicio_1/2_altura_fulano_cicrano.c
+++ romualdo/lista_exercicio_1/2_altura_fulano_cicrano.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 
+/**
+ * Programa que calcula quanto tempo demora para
+ * a altura de Cicrano ultrapaçar a altura de
+ * Fulano onde o crescimento anual respectivamente
+ * é 2 e 4.
+ **/
+
 int main() {
     int alturaFulano = 170;
     int alturaCicrano = 150;
     int crescimentoFulano = 2;
     int crescimentoCicrano = 4;
     int anos;
-    
+
     for(anos = 0; alturaCicrano <= alturaFulano; anos += 1, alturaFulano += crescimentoFulano, alturaCicrano += crescimentoCicrano) {
         printf("--- ano %d ---\n", anos);
         printf("Fulano: %d cm.\n", alturaFulano);

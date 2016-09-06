bcb38a289aa1042a0d9d3505230322e675a94f82
MrAnyone <m3rc3r99@gmail.com>
2016-09-06 08:47:28 -0300

    Add 5_somas_sucessivas.c

---
+++ romualdo/lista_exercicio_1/5_somas_sucessivas.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * Programa que calcula a multiplicação de
+ * 2 números por somas sucessivas.
+**/
+
+int main() {
+    int num1 = 0;
+    int num2 = 0;
+    int mult = 0;
+
+    printf("Insira dois numeros inteiros exemplo: '5 6'\n");
+
+    scanf("%d %d", &num1, &num2);
+
+    printf("%d * %d = ", num1, num2);
+    
+    for(num2 > 0; num2 > 0; num2 -= 1, mult += num1);
+
+    printf("%d\n", mult);
+    return 0;
+}

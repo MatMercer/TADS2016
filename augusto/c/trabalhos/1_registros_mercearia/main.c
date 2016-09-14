221dc55ca783311e123f9bc983e14d6252e997d9
MrAnyone <m3rc3r99@gmail.com>
2016-09-14 17:58:04 -0300

    Update main.c

--- augusto/c/trabalhos/1_registros_mercearia/main.c
+++ augusto/c/trabalhos/1_registros_mercearia/main.c
@@ -3,11 +3,13 @@
 
 int main() {
     Venda venda[100];
-    int quantidadeVendas = 0;
+    Data relatorioDataInicial;
+    Data relatorioDataFinal;
+    int quantidadeVendas = 0;    
     char opcao = 0;
     
     while(opcao != 's' && opcao != 'S') {
-        printf("Programa para controle vendas de uma mercearia\nOpcoes:\nv: Registrar uma venda\ns: Sair\nInsira sua opcao: ");
+        printf("Programa para controle vendas de uma mercearia\nOpcoes:\nv: Registrar uma venda\nr: Relatorio com data inicial e final\ns: Sair\nInsira sua opcao: ");
         scanf("%c", &opcao);
         __fpurge(stdin);
 
@@ -15,6 +17,14 @@ int main() {
             printf("Note que o maximo de vendas que podem ser registradas eh 100.\n");
             getVendaUsuario(&venda[quantidadeVendas]);
             quantidadeVendas += 1;
+            __fpurge(stdin);
+        }
+
+        if(opcao == 'r' || opcao == 'R') {
+            printf("Data Inicial:\n");
+            getDataUsuario(&relatorioDataInicial);
+            printf("Data Final:\n");
+            getDataUsuario(&relatorioDataFinal);
             __fpurge(stdin);
         }
     }
